652e243e9728f2c1b1168470222ab9352488aeb1
Shavkat Khamidjanov <homidjonov@gmail.com>
2014-01-28 02:26:58 +0530

    Admin UI edits

--- WebContent/static/home/js/sb-admin.js
+++ WebContent/static/home/js/sb-admin.js
@@ -1,13 +1,13 @@
-$(function() {
+$(function () {
 
     $('#side-menu').metisMenu();
 
 });
 
 //Loads the correct sidebar on window load
-$(function() {
+$(function () {
 
-    $(window).bind("load", function() {
+    $(window).bind("load", function () {
         console.log($(this).width())
         if ($(this).width() < 768) {
             $('div.sidebar-collapse').addClass('collapse')
@@ -18,14 +18,22 @@ $(function() {
 })
 
 //Collapses the sidebar on window resize
-$(function() {
+$(function () {
 
-    $(window).bind("resize", function() {
+    $(window).bind("resize", function () {
         console.log($(this).width())
         if ($(this).width() < 768) {
             $('div.sidebar-collapse').addClass('collapse')
         } else {
             $('div.sidebar-collapse').removeClass('collapse')
+        }
+    })
+})
+$(function () {
+
+    $('.btn-delete').bind("click", function () {
+        if (confirm('Are you sure delete the item?')) {
+            document.location.href = $(this).attr('action');
         }
     })
 })
